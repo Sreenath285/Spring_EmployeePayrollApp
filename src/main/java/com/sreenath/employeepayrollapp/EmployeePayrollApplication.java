0eed8e1d47db3fc43de319b074e6c803611b56a6
Sreenath285 <sreenath.285@gmail.com>
2022-05-23 18:42:46 +0530

    Used lombok library for logging

---
+++ src/main/java/com/sreenath/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,17 @@
+package com.sreenath.employeepayrollapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll App started in {} environment",
+                    applicationContext.getEnvironment().getProperty("environment"));
+    }
+}
